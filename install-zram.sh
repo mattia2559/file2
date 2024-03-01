5d64d99e94ef8a51cd849610848bf83fae84965b
mattia2559 <135450022+mattia2559@users.noreply.github.com>
2024-03-01 18:57:10 +0100

    Delete install-zram.sh

--- install-zram.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/mattia2559/file1.git
-sudo cp /home/$USER/file1/zram-generator.conf /etc/systemd
-sudo cp /home/$USER/file1/99-vm-zram-parametes.conf  /etc/sysctl.d/
-sudo systemctl start systemd-zram-setup@zram0
-
