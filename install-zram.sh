e7006a159328e6545af32b39d7cda68923f4b1ed
mattia2559 <135450022+mattia2559@users.noreply.github.com>
2024-03-01 18:49:43 +0100

    Add files via upload

---
+++ install-zram.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/mattia2559/file1.git
+sudo cp /home/$USER/file1/zram-generator.conf /etc/systemd
+sudo cp /home/$USER/file1/99-vm-zram-parametes.conf  /etc/sysctl.d/
+sudo systemctl start systemd-zram-setup@zram0
+
